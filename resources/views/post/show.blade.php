e23911bd57af0e9bd4d9a76ba1802a5070866e7b
sawadza <s3odalansari@gmail.com>
2023-08-29 11:35:27 +0300

    breadcrumbs layout fixed

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,7 +5,7 @@
     <ol class="inline-flex items-center space-x-1 md:space-x-3">
       <li class="inline-flex items-center">
         <a href="/" class="inline-flex items-center text-sm font-medium text-gray-700 hover:text-blue-600 ">
-          <svg class="w-3 h-3 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+          <svg class="w-3 h-3 mx-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="m19.707 9.293-2-2-7-7a1 1 0 0 0-1.414 0l-7 7-2 2a1 1 0 0 0 1.414 1.414L2 10.414V18a2 2 0 0 0 2 2h3a1 1 0 0 0 1-1v-4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v4a1 1 0 0 0 1 1h3a2 2 0 0 0 2-2v-7.586l.293.293a1 1 0 0 0 1.414-1.414Z"/>
           </svg>
           {{__('site.home')}}
@@ -15,9 +15,7 @@
 
       <li aria-current="page">
         <div class="flex items-center">
-          <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-          </svg>
+          /
           
           <a href="{{route('post.index')}}" class="inline-flex mx-1 items-center text-sm font-medium text-gray-700 hover:text-blue-600 "> 
             {{__('site.blogs')}}
@@ -26,10 +24,8 @@
       </li>
       <li aria-current="page">
         <div class="flex items-center">
-          <svg class="w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-          </svg>
-          <span class="me-1 text-sm font-medium text-gray-500 md:me-2 ">{{$post->title}}</span>
+          /
+          <span class="mx-1 text-sm font-medium text-gray-500 md:me-2 ">{{$post->title}}</span>
         </div>
       </li>
     </ol>
@@ -40,7 +36,11 @@
     <div class=" max-w-4xl mx-auto p-3 bg-white px-6 pt-12 text-start">
         <div class="md:ms-12 my-3">
              @foreach ($post->categories as $category)
-                <a href="{{route('post.category', $category)}}" class="inline-block hover:bg-gray-200 transition-all ease  rounded-full px-3 py-1 text-sm md:text-xl font-semibold text-gray-500">#{{$category->name}}</a>
+                @if (App::isLocale('ar') && $category->name_ar != null)
+                  <a href="{{route('post.category', $category)}}" class="inline-block hover:bg-gray-200 transition-all ease  rounded-full px-3 py-1 text-sm md:text-xl font-semibold text-gray-500">#{{$category->name_ar}}</a>
+                @else
+                  <a href="{{route('post.category', $category)}}" class="inline-block hover:bg-gray-200 transition-all ease  rounded-full px-3 py-1 text-sm md:text-xl font-semibold text-gray-500">#{{$category->name_en}}</a>
+                @endif
              @endforeach
         </div>
             
